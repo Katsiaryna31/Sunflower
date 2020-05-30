ace7d4c139713b6a41e563cbf4451f7ebd9bbbe9
Katsiaryna31 <katia310790@mail.ru>
2020-05-30 19:16:08 +0300

    add map

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,8 @@ import grass from '../assets/images/grass.png';
 import aboutusImage from '../assets/images/aboutus-photo.jpg';
 import arrow from '../assets/images/arrow.png';
 import star from '../assets/images/star.png';
+import puzzle from '../assets/images/puzzle2.png';
+import cross from '../assets/images/cross.svg';
 
 const sunflower = document.querySelector('.logo-mobile');
 sunflower.src = logo;
@@ -27,3 +29,21 @@ const starImages = document.querySelectorAll('.star');
 starImages.forEach((starImage) => {
     starImage.src = star;
 });
+
+const map = document.querySelector('.callback-image');
+map.src = puzzle;
+
+const mapContainer = document.querySelector('.callback-map');
+const mapBig = document.getElementById('map');
+
+const mapButton = document.querySelector('.map-close');
+mapButton.src = cross;
+
+mapContainer.addEventListener('click', () => {
+    mapBig.classList.add('map--active');
+})
+
+mapButton.addEventListener('click', () => {
+    mapBig.classList.remove('map--active');
+})
+
