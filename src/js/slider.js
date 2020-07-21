cd348d7480596c87bf9c405bb7c3aede42130b40
Katsiaryna31 <katia310790@mail.ru>
2020-07-21 15:13:35 +0300

    update activities page

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,26 +1,27 @@
 import Swipe from 'swipejs';
 
-window.mySwipe = new Swipe(document.querySelector('.slider'), {
-    startSlide: 0,
-    speed: 1000,
-    auto: 3000,
-    draggable: false,
-    continuous: true,
-    disableScroll: false,
-    stopPropagation: false,
-    ignore: ".scroller",
-    callback: function(index, elem, dir) {},
-    transitionEnd: function(index, elem) {}
-  });
+setTimeout(() => {
+    window.mySwipe = new Swipe(document.querySelector('.slider'), {
+        startSlide: 0,
+        speed: 1000,
+        auto: 3000,
+        draggable: false,
+        continuous: true,
+        disableScroll: false,
+        stopPropagation: false,
+        ignore: ".scroller",
+        callback: function (index, elem, dir) { },
+        transitionEnd: function (index, elem) { }
+    });
 
-const sliderLeft = document.querySelector('.slider__control_left');
-sliderLeft.addEventListener('click', ()=> {
-    mySwipe.prev();
-})
+    const sliderLeft = document.querySelector('.slider__control_left');
+    sliderLeft.addEventListener('click', () => {
+        mySwipe.prev();
+    })
 
-const sliderRight = document.querySelector('.slider__control_right');
-sliderRight.addEventListener('click', ()=> {
-    mySwipe.next();
-})
+    const sliderRight = document.querySelector('.slider__control_right');
+    sliderRight.addEventListener('click', () => {
+        mySwipe.next();
+    })
 
-
+}, 100)
