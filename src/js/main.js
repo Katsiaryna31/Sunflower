1756d9c46b1f84ae13ca5424d04f067d47467d9a
Katsiaryna31 <katia310790@mail.ru>
2020-10-16 13:06:49 +0300

    add spiner and popup to form

--- src/js/main.js
+++ src/js/main.js
@@ -78,16 +78,34 @@ activitiesLink.addEventListener('click', (e) => {
 })
 
 const contactForm = document.getElementById('sendmailform');
+const spiner = document.querySelector('.lds-spinner');
+
+
+const popupOpen = (message) => {
+    spiner.style.display = 'none';
+    const popup = document.getElementById('popup');
+    const popupText = document.querySelector('.popup-text');
+    const popupButton = document.querySelector('.popup-button');
+    popup.style.display = 'block';
+    popupText.innerHTML = message;
+    popupButton.addEventListener('click', () => {
+        popup.style.display = 'none';
+        contactForm.reset();
+    })
+}
+
+
 contactForm.addEventListener('submit', (e) => {
     e.preventDefault();
     console.log('submit');
+    spiner.style.display = 'inline-block';
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "/sendmail.php");
     xhr.onload = function (event) {
-        alert("Повідомлення надіслано");
+        popupOpen("Повідомлення надіслано");
     };
     xhr.onerror = function (event) {
-        alert("Виникла помилка. Спробуй пізніше");
+        popupOpen("Виникла помилка. Спробуй пізніше");
     };
     // or onerror, onabort
     var formData = new FormData(document.getElementById("sendmailform"));
